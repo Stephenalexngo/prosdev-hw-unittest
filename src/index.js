dba20dc602eb2b932808ee3f77543b00429b9c12
Stephenalexngo <stephenalexngo@gmail.com>
2020-07-16 18:43:34 +0800

    added unit test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 const functions = {
-  gradeClassifier: (grade) => {
-    // TODO: Write code here
-  }
+    gradeClassifier: (grade) => {
+        if (grade <= 100 && grade >= 95)
+            return "O - Oustanding";
+        else if (grade < 95 && grade >= 90)
+            return "V - Very Good";
+        else if (grade < 90 && grade >= 85)
+            return "G - Good";
+        else if (grade < 85 && grade >= 80)
+            return "S - Satisfactory";
+        else if (grade < 80 && grade >= 75)
+            return "N - Needs Improvement";
+        else
+            return "D - Did not Meet Expectation";
+    }
 }
 
 module.exports = functions;
--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,45 @@
 const { gradeClassifier } = require('./index');
 
-// TODO: Write unit tests here
-describe('Scenario', () => {
-  it('When ..., then ...', () => {
-    // Arrange
+describe('Grade Classifier Group', () => {
+    it('Should Show Oustanding Grade', () => {
+        var grade = 96;
+        var expectedString = "O - Oustanding";
 
-    // Act
+        expect(gradeClassifier(grade)).toBe(expectedString)
+    });
 
-    // Assert
-  });
+    it('Should Show Very Good Grade', () => {
+        var grade = 92;
+        var expectedString = "V - Very Good";
+
+        expect(gradeClassifier(grade)).toBe(expectedString)
+    });
+
+    it('Should Show Good Grade', () => {
+        var grade = 87;
+        var expectedString = "G - Good";
+
+        expect(gradeClassifier(grade)).toBe(expectedString)
+    });
+
+    it('Should Show Satisfactory Grade', () => {
+        var grade = 84;
+        var expectedString = "S - Satisfactory";
+
+        expect(gradeClassifier(grade)).toBe(expectedString)
+    });
+
+    it('Should Show Needs Improvement Grade', () => {
+        var grade = 77;
+        var expectedString = "N - Needs Improvement";
+
+        expect(gradeClassifier(grade)).toBe(expectedString)
+    });
+
+    it('Should Show Did not Meet Expectation Grade', () => {
+        var grade = 55;
+        var expectedString = "D - Did not Meet Expectation";
+
+        expect(gradeClassifier(grade)).toBe(expectedString)
+    });
 });
